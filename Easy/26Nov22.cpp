ae42b2b696f29ebddd34d51384ca80cec33e7901
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-11-26 09:46:37 +0530

    Create 26Nov22.cpp

---
+++ Easy/26Nov22.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+void rearrangeArray(int arr[], int n)
+{
+  for(int i=0;i<n;i++){
+    arr[i]+= (arr[arr[i]]%n)*n;
+  }
+    for(int i=0;i<n;i++){
+        arr[i]/=n;
+    }
+}
